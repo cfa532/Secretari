925b8323439cac765b138d0a18a6324359d5413c
cfa532 <cfa532@yahoo.com>
2024-07-01 21:01:13 +0800

    EULA

--- Secretari/View/Settings/HelpView.swift
+++ Secretari/View/Settings/HelpView.swift
@@ -20,6 +20,9 @@ struct HelpView: View {
                     Text("This app generates summaries from user speech transcripts by utilizing a top-tier AI service. There are two types of summaries available: a standard summary and a checklist-style summary, referred to as a Memo. You can select the desired type by choosing the Prompt Type in the Settings. Both types of summaries can be manually edited to correct any errors.")
                 }
                 Section(header: Text("Policy")) {
+                    HStack {
+                        Text(getEula)
+                    }
                     Text("Regarding cancellations and refunds, this app adheres to the policies of the Apple Store. Additionally, there is a usage cap for subscribers, limiting their monthly expenses to approximately twice the amount we pay OpenAI for using its API. If a subscriber exceeds this limit, they can make a one-time purchase of tokens to continue using the app.")
                 }
             }
@@ -30,6 +33,12 @@ struct HelpView: View {
         }
     }
     
+    private var getEula: AttributedString {
+        var text = AttributedString("We follow the same Terms of Use (EULA) as Apple.")
+        text[text.range(of: "Terms of Use (EULA)")!].underlineStyle = .single
+        text[text.range(of: "Terms of Use (EULA)")!].link = URL(string: "https://www.apple.com/legal/macapps/stdeula/")!
+        return text
+    }
     private func loadNotice() {
         Task { @MainActor in
             if let fetchedNotice = try await Websocket.shared.getNotice() {
